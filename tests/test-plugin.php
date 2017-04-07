c62118ebbbaaa8490b799d0bea4b36f1c4d19949
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-04-07 03:15:39 +0200

    Config basic unit tests (plugin activation)

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -1,9 +1,16 @@
 <?php
+/**
+ * Widget Subtitles - Unit tests
+ *
+ * @author  Jory Hogeveen <info@keraweb.nl>
+ * @package Widget_Subtitles
+ */
+
 
 class PluginTest extends WP_UnitTestCase {
 
-  // Check that that activation doesn't break
-  function test_plugin_activated() {
-    $this->assertTrue( is_plugin_active( PLUGIN_PATH ) );
-  }
+	// Check that that activation doesn't break
+	function test_plugin_activated() {
+		$this->assertTrue( is_plugin_active( TEST_WS_PLUGIN_PATH ) );
+	}
 }
