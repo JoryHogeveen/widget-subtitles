5a1b5b0309470d6b980f2e6a69823a27b354631c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-04-07 16:36:35 +0200

    add_subtitle() unit tests

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -13,4 +13,108 @@ class PluginTest extends WP_UnitTestCase {
 	function test_plugin_activated() {
 		$this->assertTrue( is_plugin_active( TEST_WS_PLUGIN_PATH ) );
 	}
+
+	function test_add_subtitle() {
+		$ws = ws_widget_subtitles();
+
+		$tests = array(
+			array(
+				'start'  => array(),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => ' <div>SUBTITLE</div>',
+					'before_title' => '',
+				),
+				'location' => null, // default
+			),
+			array(
+				'start'  => array(),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => '',
+					'before_title' => '<div>SUBTITLE</div> ',
+				),
+				'location' => 'before-inside', // default
+			),
+			array(
+				'start'  => array(
+					'after_title' => '</div>',
+					'before_title' => '<div class="title">',
+				),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => '</div>',
+					'before_title' => '<div class="title"><div>SUBTITLE</div> ',
+				),
+				'location' => 'before-inside', // default
+			),
+			array(
+				'start'  => array(),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => '',
+					'before_title' => '<div>SUBTITLE</div>',
+				),
+				'location' => 'before-outside', // default
+			),
+			array(
+				'start'  => array(
+					'after_title' => '</div>',
+					'before_title' => '<div class="title">',
+				),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => '</div>',
+					'before_title' => '<div>SUBTITLE</div><div class="title">',
+				),
+				'location' => 'before-outside', // default
+			),
+			array(
+				'start'  => array(),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => ' <div>SUBTITLE</div>',
+					'before_title' => '',
+				),
+				'location' => 'after-inside', // default
+			),
+			array(
+				'start'  => array(
+					'after_title' => '</div>',
+					'before_title' => '<div class="title">',
+				),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => ' <div>SUBTITLE</div></div>',
+					'before_title' => '<div class="title">',
+				),
+				'location' => 'after-inside', // default
+			),
+			array(
+				'start'  => array(),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => '<div>SUBTITLE</div>',
+					'before_title' => '',
+				),
+				'location' => 'after-outside', // default
+			),
+			array(
+				'start'  => array(
+					'after_title' => '</div>',
+					'before_title' => '<div class="title">',
+				),
+				'data'   => '<div>SUBTITLE</div>',
+				'result' => array(
+					'after_title' => '</div><div>SUBTITLE</div>',
+					'before_title' => '<div class="title">',
+				),
+				'location' => 'after-outside', // default
+			),
+		);
+
+		foreach ( $tests as $test ) {
+			$this->assertEquals( $test['result'], $ws->add_subtitle( $test['start'], $test['data'], $test['location'] ) );
+		}
+	}
 }
